536574931c7fbe42756bca7c818671e9e917bafe
sarinasalim <slnsalim@gmail.com>
2015-01-15 01:03:48 +0800

    Added code to query Twitter API. New end point /Home/TwitterFeeds returns LR tweets in JSON format

---
+++ Libraries/Nop.Core/Social/TwitterManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Helpers;
+using Tweetinvi;
+using Tweetinvi.Core.Interfaces;
+using Tweetinvi.Json;
+
+namespace Nop.Core.Social
+{
+    /// <summary>
+    /// Make calls to the Twitter API via this Manager which utilses the Tweetinvi library
+    /// https://tweetinvi.codeplex.com
+    /// </summary>
+    public class TwitterManager
+    {
+        public TwitterManager(string accessToken, string accessTokenSecret, string consumerKey, string consumerSecret )
+        {
+            TwitterCredentials.SetCredentials(accessToken, accessTokenSecret, consumerKey, consumerSecret);
+        }
+
+        public string GetUserTimeline(string userScreenName, int maximumTweets)
+        {
+            var tweets = TimelineJson.GetUserTimeline(userScreenName, maximumTweets);
+            return tweets;
+        }
+    }
+}
--- Presentation/Nop.Web/Controllers/HomeController.cs
+++ Presentation/Nop.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+using Nop.Core.Social;
 using Nop.Web.Framework.Security;
 
 namespace Nop.Web.Controllers
@@ -9,6 +12,26 @@ namespace Nop.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public string TwitterFeeds()
+        {
+            var tm = new TwitterManager(
+                           ConfigurationManager.AppSettings["Twitter-Access-Token"],
+                           ConfigurationManager.AppSettings["Twitter-Access-Token-Secret"],
+                           ConfigurationManager.AppSettings["Twitter-Consumer-Key"],
+                           ConfigurationManager.AppSettings["Twitter-Consumer-Secret"]
+                           );
+
+            int maximumTweets;
+            if (!Int32.TryParse(ConfigurationManager.AppSettings["Twitter-Consumer-Secret"], out maximumTweets))
+            {
+                maximumTweets = 5;
+            }
+
+            var tweets = tm.GetUserTimeline(ConfigurationManager.AppSettings["Twitter-ScreenName"], maximumTweets);
+            
+            return tweets;
         }
     }
 }
