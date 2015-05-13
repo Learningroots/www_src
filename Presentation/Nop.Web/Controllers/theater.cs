86db5089b4c2ef764bff0a1a141a517421df9884
Aamir Afridi <aamirafridi@gmail.com>
2015-05-13 14:11:00 -0700

    header done

---
+++ Presentation/Nop.Web/Controllers/theater.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nop.Web.Controllers
+{
+    public class Theater : Controller
+    {
+        // GET: theater
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
